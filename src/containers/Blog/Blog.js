bc345983365989e7b57e46797ea0bf1916c526eb
MaciejX, Marcinkowski <marcin1x>
2020-09-27 15:21:44 +0200

    Implement DELETE and POST

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,6 +9,7 @@ import "./Blog.css";
 class Blog extends Component {
   state = {
     posts: [],
+    selectedPostId: null,
   };
 
   componentDidMount() {
@@ -24,16 +25,35 @@ class Blog extends Component {
     });
   }
 
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  };
+
+  postDeletedHandler = () => {
+    console.log("Exected from fullpost")
+    this.setState({ selectedPostId: null });
+  };
+
   render() {
     const posts = this.state.posts.map((post) => {
-      return <Post key={post.id} title={post.title} author={post.author}/>;
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      );
     });
 
     return (
       <div>
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost
+            id={this.state.selectedPostId}
+            deleteId={this.postDeletedHandler}
+          />
         </section>
         <section>
           <NewPost />
